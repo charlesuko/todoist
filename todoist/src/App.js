015521c02227b44f5d7b6dc4e87099e8d1caa813
Charles Uko <charlesuko@Charless-MBP.lan>
2020-10-04 20:47:54 -0400

    Configured the react app and added directories

---
+++ todoist/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello there!</p>
+      </header>
+    </div>
+  );
+}
+
+
